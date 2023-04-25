32480d5f2a1f284dcab50635efb88b38494fa80c
MalshaniBandaranayake <malshanibandaranayake91@gmail.com>
2023-04-25 09:04:23 +0530

    java static codes

---
+++ src/Teststatic.java
@@ -0,0 +1,12 @@
+public class Teststatic {
+    static int age;
+    static void show(){
+        System.out.println("this is static method");
+    }
+
+    public static void main(String[] args) {
+        age = 25;
+        System.out.println("age is:"+age);
+        show();
+    }
+}
